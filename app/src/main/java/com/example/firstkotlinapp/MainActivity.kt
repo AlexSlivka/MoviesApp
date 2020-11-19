b3c01b1684485fbb6b86783f14f182916ab0146f
Aleksei Slivka <alexprospero25@gmail.com>
2020-11-19 22:36:24 +0300

    Начал верстку экрана с данными о фильме. Сделал переключение с главного экрана на экран с данными о фильме и обратно.

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.firstkotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.to_movie_details_textVew)
+        textView.setOnClickListener{moveToMovieDetailsScreen()}
+    }
+    private fun moveToMovieDetailsScreen(){
+        val intent = Intent(this,MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
