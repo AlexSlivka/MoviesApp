a672e4e6ac023ee13c4a21a5b2218466936c2b6d
Aleksei Slivka <alexprospero25@gmail.com>
2020-11-29 16:50:06 +0300

    Добавил фрагменты fragment_movies_list и fragment_movies_detail. Удалил активити MoviesDetail.

---
+++ app/src/main/java/com/example/firstkotlinapp/FragmentMoviesDetails.kt
@@ -0,0 +1,26 @@
+package com.example.firstkotlinapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesDetails : Fragment() {
+    private var btnBackToMovieList: TextView? = null
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movies_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnBackToMovieList = view.findViewById(R.id.back_textView)
+
+    }
+}
---
+++ app/src/main/java/com/example/firstkotlinapp/FragmentMoviesList.kt
@@ -0,0 +1,26 @@
+package com.example.firstkotlinapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+    private var btnToMovieDetails: ImageView? = null
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnToMovieDetails = view.findViewById(R.id.poster_avengers_end_game_imageView)
+    }
+}
