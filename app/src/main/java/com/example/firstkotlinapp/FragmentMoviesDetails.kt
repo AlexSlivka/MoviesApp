d5b799f9bc2f6ecc294580e25b7636db1de08034
Aleksei Slivka <alexprospero25@gmail.com>
2020-12-24 22:12:56 +0300

    Добавил ratingBar в MovieDetail.

--- app/src/main/java/com/example/firstkotlinapp/FragmentMoviesDetails.kt
+++ app/src/main/java/com/example/firstkotlinapp/FragmentMoviesDetails.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,6 +24,8 @@ class FragmentMoviesDetails : Fragment() {
         view.findViewById<TextView>(R.id.back_textView).setOnClickListener {
             fragmentManager?.popBackStack()
         }
+        val ratingStar:RatingBar = view.findViewById(R.id.ratingBar_star)
+        ratingStar.rating= 4F
         return view
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
